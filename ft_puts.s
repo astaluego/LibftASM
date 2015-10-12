4bb0c11c412fd6b75f3fbfd7de2e8bff223a4afa
Anastasia DERUELLE <aderuell@e2r10p23.42.fr>
2015-10-12 16:48:52 +0200

    puts : missing new line

--- ft_puts.s
+++ ft_puts.s
@@ -2,30 +2,35 @@
 %define WRITE				4
 %define STDOUT				1
 %define EOF					-1
+%define OK					10
 
 section .data
 nl:
-	db 10
+	.string db 10
+	.len equ $ - nl.string
+nll:
+	db "(null)"
 
 section .text
 	global _ft_puts
 	extern _ft_strlen
 	
 _ft_puts:
+	cmp rdi, 0x0
+	je null
+print:
 	mov r10, rdi
 	call _ft_strlen
-	cmp rax, 0x0
-	je end
-print:
+	mov rdi, STDOUT						;output
 	mov rsi, r10						;string
 	mov rdx, rax						;len
-	mov rdi, STDOUT						;output
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
 	jc error
-	js error
-	lea rsi, [rel nl]					;string
-	mov rdx, 0x1						;len
+	js error	
+	mov rdi, STDOUT						;output
+	lea rsi, [rel nl.string]			;string
+	mov rdx, nl.len						;len
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
 	jc error
@@ -33,8 +38,10 @@ print:
 	jmp end
 error:
 	mov rax, EOF
-	leave
 	ret
 end:
-	leave
+	mov rax, OK
 	ret
+null:
+	lea rdi, [rel nll]
+	jmp print
