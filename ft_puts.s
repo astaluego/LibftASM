4b52b393e49c773f260897decc964b6a44e42814
Anastasia DERUELLE <aderuell@e3r6p20.42.fr>
2015-10-13 15:43:09 +0200

    fix puts

--- ft_puts.s
+++ ft_puts.s
@@ -4,44 +4,48 @@
 %define EOF					-1
 %define OK					10
 
-section .data
-nl:
-	.string db 10
-	.len equ $ - nl.string
+section .rodata
 nll:
-	db "(null)"
+	db "(null)", 10 
 
 section .text
 	global _ft_puts
 	extern _ft_strlen
 	
 _ft_puts:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
 	cmp rdi, 0x0
 	je null
 print:
-	mov r10, rdi
+	mov r11, rdi
 	call _ft_strlen
 	mov rdi, STDOUT						;output
-	mov rsi, r10						;string
+	mov rsi, r11						;string
 	mov rdx, rax						;len
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
-	jc error
-	js error	
-	mov rdi, STDOUT						;output
-	lea rsi, [rel nl.string]			;string
-	mov rdx, nl.len						;len
+	lea rsi, [rel nll + 6]				;string
+	mov rdx, 0x1						;len
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
 	jc error
 	js error
+	mov rax, OK
 	jmp end
 error:
 	mov rax, EOF
-	ret
 end:
-	mov rax, OK
+	leave
 	ret
 null:
-	lea rdi, [rel nll]
-	jmp print
+	mov rdi, STDOUT						;output
+	lea rsi, [rel nll]
+	mov rdx, 0x7						;len
+	mov rax, MACH_SYSCALL(WRITE)
+	syscall
+	jc error
+	js error
+	mov rax, OK
+	jmp end
