a96d64e986de23b65224687502c04474a4e0fd17
Anastasia DERUELLE <aderuell@e3r6p8.42.fr>
2015-10-20 13:59:05 +0200

    fix cat

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -2,9 +2,10 @@
 %define READ				3
 %define WRITE				4
 %define	STDOUT				1
+%define BUFF				20
 
 section .bss
-	buffer resb 1024
+	buffer resb 20
 
 section .text
 	global _ft_cat
@@ -12,13 +13,18 @@ section .text
 _ft_cat:
 	push rbp
 	mov rbp, rsp
+	mov r15, rdi
+while:
+	mov rdi, r15
 	lea rsi, [rel buffer]
-	mov rdx, 1024
+	mov rdx, BUFF
 	mov rax, MACH_SYSCALL(READ)
 	syscall
 	mov rdi, STDOUT
 	mov rdx, rax
-	mov rax, MACH_SYSCALL(WRITE) 
-	syscall 
+	mov rax, MACH_SYSCALL(WRITE)
+	syscall
+	cmp rax, BUFF
+	je while
 	leave
 	ret
